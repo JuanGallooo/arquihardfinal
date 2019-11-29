fe0c1f4feb222953c7e6bb58f6ef4ce9aae6a3fc
Manuel Quintero <quinteromanuel1202@gmail.com>
2019-11-28 23:27:58 -0500

    Distancia Chi Cuadrado

---
+++ Entrega/ProyectoFinalArquiHard/model/DistanciaChi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalArquiHard.model
+{
+    class DistanciaChi
+    {
+
+        public void DistanciaChiCuadrado(byte[,] imagen1, byte[,] imagen2)
+        {
+            int width = imagen1.GetLength(0);
+            int length = imagen2.GetLength(1);
+            double result = 0;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    int x = imagen1[i, j];
+                    int y = imagen2[i, j];
+
+                    double dist = ((x - y) ^ 2) / (x + y);
+
+                    result += dist;
+
+                }
+            }
+        }
+    }
+}
